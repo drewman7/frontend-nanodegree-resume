955061551e145227afd73707cb96bfab5d3c87c6
Drew <drewstuff5@hotmail.com>
2014-12-20 11:04:26 -0500

    End of Lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,87 @@
-var formattedName = 
-	HTMLheaderName.replace("%data%","Andrew Totten");
+var bio = {
+	"name":"Andrew Totten",
+	"role":"Web Developer",
+	"contacts": {
+		"mobile":"989-615-8981",
+		"email":"drewstuff5@hotmail.com",
+		"twitter": "@drewtott"
+	},
+	"welcomemsg":"Welcome to my resume",
+	"skills": ["Crazy", "Cool", "Free"],
+	"bioPic": "images/drew.jpg"
+};
 
-var formattedRole = 
-	HTMLheaderRole.replace("%data%","Web Developer");
+var work = {
+	"jobs": [
+		{
+			"employer": "AT&T",
+			"title": "Sr. Solutions Architect",
+			"location": "Saginaw, MI, US",
+			"dates": "1996 to Present",
+			"description": "Designing and solutioning of voice over IP solutions for customers."
+		},
+		{
+			"employer": "Nortel",
+			"title": "CAST Lab Engineer",
+			"location": "Plano, TX, US",
+			"dates": "1956 to 1996",
+			"description": "Testing and evaluating Nortel products and solutions."
+		}
+	]
+};
 
-$("#header").prepend(formattedName,formattedRole);
+var projects = {
+	"projects": [
+		{
+			"title": "Project 1 - HTML and CSS Design",
+			"dates": "2014",
+			"description": "Project 1 for the Frontend Web Design Nanodegree.",
+			"images": "images/mug.png"
+		},
+		{
+			"title": "Project 2 - JavaScript Design",
+			"dates": "2014",
+			"description": "Project 2 for the Frontend Web Design Nanodegree.",
+			"images": "images/wakko.bmp"
+		}
+	]
+};
 
-print(formattedName);
+var education = {
+	"schools": [
+		{
+			"name": "University of Michigan",
+			"degree": "Bachelor of Science",
+			"majors": ["Electrical Engineering"],
+			"dates": 1995,
+			"city": "Ann Arbor, MI, US",
+			"url": "http://umich.edu"
+		},
+		{
+			"name": "Nouvel Catholic Central High School",
+			"dates": 1990,
+			"city": "Saginaw, MI, US",
+			"url": "http://www.saginaw.org/catholic-schools/saginaw/nouvel-catholic-central-high-school/visit.html"	
+		}
+	],
+	"onlineCourses": [
+		{
+			"name": "Udacity",
+			"degree": "Nanodegree",
+			"majors": "Front End Web Design",
+			"dates": 2015,
+			"url": "http://udacity.com"
+		}
+	]
+};
+
+//var formattedName = 
+//	HTMLheaderName.replace("%data%",bio.name);
+
+//var formattedRole = 
+//	HTMLheaderRole.replace("%data%",bio.role);
+
+//$("#header").prepend(formattedName,formattedRole);
+//$("#main").append(bio.contacts.mobile);
+
+//$("#main").append(<img src=bio.bioPic>);
